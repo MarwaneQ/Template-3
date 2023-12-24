c7ec5300612fed96e7aa934d8797528a1685e9f7
MarwaneQ <merouane12345bensadallah@gmail.com>
2023-12-23 16:59:15 -0800

    countDown with JS

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-window.addEventListener('scroll', function() {
-    console.log('Page scrolled!');
-});
+let countDown=new Date("Dec 31,2024 23:59:59").getTime()
+let counter=setInterval(() =>{
+    let datNow=new Date().getTime()
+    let dateDiff= countDown - datNow; 
+    let days= Math.floor(dateDiff /(1000*60*60*24))
+    let hours= Math.floor((dateDiff % (1000*60*60*24))/(1000*60*60))
+    let minutes= Math.floor((dateDiff % (1000*60*60))/(1000*60))
+    let seconds = Math.floor((dateDiff % (1000*60))/1000)
+    document.querySelector(".days").innerHTML=days
+    document.querySelector(".hours").innerHTML=hours
+    document.querySelector(".minutes").innerHTML=minutes
+    document.querySelector(".seconds").innerHTML=seconds
+    if(dateDiff<0){
+        clearInterval(counter)
+    }
+},1000)
